17a2af456c89a0c5c0026d0fd07d7f6791692fc6
Fabio Ayres <fabioja@insper.edu.br>
2024-04-21 11:48:30 -0300

    removed .env generation

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,12 +6,7 @@ from pathlib import Path
 def main():
     project_dir = Path.cwd()
     data_dir = project_dir / 'data'
-
     data_dir.mkdir(exist_ok=True)
-
-    with open('.env', 'w', encoding='utf8') as f:
-        f.write(f'PROJECT_DIR={project_dir}\n')
-        f.write(f'DATA_DIR={data_dir}\n')
 
     subprocess.run(['git', 'init'], check=True)
     subprocess.run(['git', 'add', '.'], check=True)
