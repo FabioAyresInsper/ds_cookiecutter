d2f2d87840767ab4a2d2ce3dfc64e2a28a490a1f
Fabio Ayres <fabioja@insper.edu.br>
2024-04-25 12:51:48 -0300

    blablabla

--- {{ cookiecutter.project_slug }}/docker/scripts/run_container.py
+++ {{ cookiecutter.project_slug }}/docker/scripts/run_container.py
@@ -1,35 +1,27 @@
-"""A script to run our Docker Data Science container.
-
-This script instantiates a Docker Data Science container with .
-
-Usage:
-    $ python xyz_script.py [options]
-
-Options:
-    -h, --help          Show this help message and exit.
-    -i, --input FILE    Input file containing data (default: stdin).
-    -o, --output FILE   Output file to write results (default: stdout).
-    -v, --verbose       Increase verbosity level.
-
-Examples:
-    $ python xyz_script.py -i input.txt -o output.txt
-    This command processes data from input.txt and writes results to output.txt.
-
-    $ python xyz_script.py -v
-    This command runs the script in verbose mode, providing more detailed output.
-
-Author: Your Name <your.email@example.com>
-"""
-
-import argparse
+# pylint: disable=missing-docstring
+import subprocess
+from pathlib import Path
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
+
+    project_name = '{{cookiecutter.project_slug}}'
+
+    source_dir = Path(__file__).parents[2]
+    target_dir = f'/home/user/{project_name}'
+    subprocess.run(
+        [
+            'docker',
+            'run',
+            '-it',
+            '--mount',
+            f'type=bind,source={source_dir},target={target_dir}',
+            '--name',
+            'projeto',
+            'projeto',
+        ],
+        check=False,
     )
-    parser.parse_args()
 
 
 if __name__ == "__main__":
--- {{ cookiecutter.project_slug }}/docker/scripts/run_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run \
-    -it \
-    --mount type=bind,source=/path/to/your/dataset,target=/mnt/data \
-    --name $1 \
-    $2
-    
